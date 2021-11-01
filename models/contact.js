01aa5434a08bfbd5704dfe16160a349ea8c748af
ManniSingh <manny.chadha@gmail.com>
2021-11-02 01:16:12 +0200

    0.2

--- models/contact.js
+++ models/contact.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const url = process.env.MONGODB_URI
 
@@ -13,9 +14,11 @@ mongoose.connect(url)
   })
 
 const pbSchema = new mongoose.Schema({
-name: String,
-number: String
+name: { type: String, required: true, unique: true },
+number: { type: String, required: true}
 })
+
+pbSchema.plugin(uniqueValidator)
 
 pbSchema.set('toJSON', {
   transform: (document, returnedObject) => {
